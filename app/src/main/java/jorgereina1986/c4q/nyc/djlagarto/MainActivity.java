52a3ed818123fbdecddc095752112e09d1922f3e
jorgereina1986 <jorgereina1986@gmail.com>
2016-08-30 01:26:37 -0400

    edited layout and audio streaming ok

--- app/src/main/java/jorgereina1986/c4q/nyc/djlagarto/MainActivity.java
+++ app/src/main/java/jorgereina1986/c4q/nyc/djlagarto/MainActivity.java
@@ -3,7 +3,6 @@ package jorgereina1986.c4q.nyc.djlagarto;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,21 +16,10 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import jorgereina1986.c4q.nyc.djlagarto.model.Track;
 import jorgereina1986.c4q.nyc.djlagarto.model.TrackResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -45,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String CLIENT_ID = BuildConfig.CLIENT_ID;
     private static final String BASE_URL = "https://api.soundcloud.com/";
     private ListView mListView;
-    private List<Track> resultList;
+    private List<TrackResponse> resultList;
     private CustomAdapter adapter;
     private Context context;
     private Toolbar mToolbar;
@@ -62,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.test);
         // Media Player setup
         mMediaPlayer = new MediaPlayer();
         mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -84,8 +71,8 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         mMediaPlayerToolbar = (Toolbar) findViewById(R.id.toolbar2);
-        mSelectedTrackTitle = (TextView)findViewById(R.id.current_track_tv);
-        mSelectedTrackImage = (ImageView)findViewById(R.id.current_track_iv);
+        mSelectedTrackTitle = (TextView) findViewById(R.id.current_track_tv);
+        mSelectedTrackImage = (ImageView) findViewById(R.id.current_track_iv);
         mPlayerControl = (ImageView) findViewById(R.id.player_control_iv);
 
         // Play/Pause Button
@@ -103,120 +90,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (mMediaPlayer != null) {
-            if (mMediaPlayer.isPlaying()) {
-                mMediaPlayer.stop();
-            }
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-        }
-
-    }
-
-    public class MusicTask extends AsyncTask<String,Void, List<Track> > {
-
-
-        @Override
-        protected List<Track> doInBackground(String... params) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-                String line ="";
-                while ((line = reader.readLine()) != null){
-                    buffer.append(line);
-                }
-
-                String finalJson = buffer.toString();
-
-                JSONArray array = new JSONArray(finalJson);
-
-                List<Track> trackList = new ArrayList<>();
-
-                for(int i=0; i<array.length(); i++) {
-                    JSONObject finalObject = array.getJSONObject(i);
-
-                    Track track = new Track();
-                    track.setTitle(finalObject.getString("title"));
-                    track.setImageUrl(finalObject.getString("artwork_url"));
-                    track.setTrackDuration(finalObject.getLong("duration"));
-                    track.setStreamUrl(finalObject.getString("stream_url"));
-
-
-                    // adding the final object in the list
-                    trackList.add(track);
-                }
-                return trackList;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } finally {
-                if(connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if(reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return  null;
-        }
-
-        @Override
-        protected void onPostExecute(final List<Track> result) {
-            super.onPostExecute(result);
-            if(result != null) {
-//                adapter = new CustomAdapter(getApplicationContext(), result);
-                mListView.setAdapter(adapter);
-
-                mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Track track = result.get(position);
-
-                        // Loading info to Media Player
-                        mSelectedTrackTitle.setText(track.getTitle());
-                        Picasso.with(context).load(track.getImageUrl()).into(mSelectedTrackImage);
-                        Toast.makeText(getApplicationContext(), "You clicked on "+ track.getStreamUrl(), Toast.LENGTH_SHORT).show();
-
-                        if (mMediaPlayer.isPlaying()) {
-                            mMediaPlayer.stop();
-                            mMediaPlayer.reset();
-                            mMediaPlayer.start();
-                        }
-
-                        try {
-                            mMediaPlayer.setDataSource(track.getStreamUrl()+"?client_id="+CLIENT_ID);
-                            mMediaPlayer.prepareAsync();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                });
-            } else {
-                Toast.makeText(context, "Not able to fetch data from server, please check url.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
     private void togglePlayPause() {
         if (mMediaPlayer.isPlaying()) {
             mMediaPlayer.pause();
@@ -227,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void networkRequest(){
+    private void networkRequest() {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -241,24 +114,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<TrackResponse>> call, Response<List<TrackResponse>> response) {
 
-                Log.d(TAG, "response: "+ response.body().get(1).getTitle());
-
-                textView.setText(response.body().get(1).getTitle());
-
-                List<TrackResponse> tracksList = response.body();
-
-                adapter = new CustomAdapter(getApplicationContext(), tracksList);
+                Log.d(TAG, "response: " + response.body().get(1).getTitle());
+                resultList = response.body();
+                adapter = new CustomAdapter(getApplicationContext(), resultList);
                 mListView.setAdapter(adapter);
 
-
+                onTrackClicked();
             }
 
             @Override
             public void onFailure(Call<List<TrackResponse>> call, Throwable t) {
 
-                textView.setText(t+"");
+                Toast.makeText(getApplicationContext(), t+"", Toast.LENGTH_SHORT);
             }
         });
     }
+
+    private void onTrackClicked() {
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TrackResponse track = resultList.get(position);
+
+                // Loading info to Media Player
+                mSelectedTrackTitle.setText(track.getTitle());
+                Picasso.with(context).load(track.getArtworkUrl()).into(mSelectedTrackImage);
+                Toast.makeText(getApplicationContext(), "You clicked on " + track.getStreamUrl(), Toast.LENGTH_SHORT).show();
+
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                    mMediaPlayer.reset();
+                    mMediaPlayer.start();
+                }
+
+                try {
+                    mMediaPlayer.setDataSource(track.getStreamUrl() + "?client_id=" + CLIENT_ID);
+                    mMediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+    }
+
 
 }
