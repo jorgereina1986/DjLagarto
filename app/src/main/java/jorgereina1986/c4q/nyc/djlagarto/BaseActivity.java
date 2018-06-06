ec3c37f32c03bc69681853e8aec1dae371c091e2
jorgereina1986 <jorgereina1986@gmail.com>
2018-06-06 00:03:04 -0400

    added data binding to activities

--- app/src/main/java/jorgereina1986/c4q/nyc/djlagarto/BaseActivity.java
+++ app/src/main/java/jorgereina1986/c4q/nyc/djlagarto/BaseActivity.java
@@ -1,49 +1,47 @@
 package jorgereina1986.c4q.nyc.djlagarto;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import jorgereina1986.c4q.nyc.djlagarto.databinding.ActivityBaseBinding;
+
 public class BaseActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
-    private PagerAdapter adapter;
-    private TabLayout tabLayout;
-    private ActionBar actionBar;
+    private ActivityBaseBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_base);
         bindViews();
 
         if (!isNetworkAvailable(this)) {
-            Toast.makeText(this, "Network error. Please make sure you are connected to the internet", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.netwrok_error_text, Toast.LENGTH_LONG).show();
         }
     }
 
     private void bindViews() {
 
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
-        adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
-        actionBar = getSupportActionBar();
-        actionBar.setTitle("Home");
+        PagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        binding.viewPager.setAdapter(adapter);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.action_bar_home_text);
+        }
     }
 
     public boolean isNetworkAvailable(final Context context) {
-        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
-        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+        final ConnectivityManager connectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager != null &&
+                connectivityManager.getActiveNetworkInfo() != null &&
+                connectivityManager.getActiveNetworkInfo().isConnected();
     }
-
-
-
 }
